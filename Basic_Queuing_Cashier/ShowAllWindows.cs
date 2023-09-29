fb05ca5133c706fb83cd864883237d7fc59c8a1c
Madonza.RicsonJay <ricsonjaymadonza@gmail.com>
2023-09-29 20:17:31 +0800

    Add project files.

---
+++ Basic_Queuing_Cashier/ShowAllWindows.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Basic_Queuing_Cashier
+{
+    class ShowAllWindows : ApplicationContext
+    {
+            private int openForms;
+            public ShowAllWindows(params Form[] forms)
+            {
+                openForms = forms.Length;
+
+                foreach (var form in forms)
+                {
+                    form.FormClosed += (s, args) =>
+                    {
+                        if (Interlocked.Decrement(ref openForms) == 0)
+                            ExitThread();
+                    };
+
+                    form.Show();
+                }
+            }
+    }
+}
